d4c0b410e74845946f09f27e7f5621bcbfa8650c
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-06-06 00:16:17 +0200

    remove useless stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+function LunishNl({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    )
+};
 
-export default MyApp
+export default LunishNl;
